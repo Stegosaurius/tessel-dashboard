143ccd200ab708f74efb6b331bf715c664460a6a
Barry Blaha <barryblaha@gmail.com>
2015-10-16 15:18:53 -0700

    Bug fixes so that the tessel will deploy on USB battery

--- index.js
+++ index.js
@@ -2,11 +2,15 @@
 var Keen = require('keen.io');
 var wifi = require('wifi-cc3000');
 var keenConfigure = require('./assets/js/keenConfigure.js');
-var wifiSettings = {ssid: 'Hack Reactor', password:'awesomebullets'};
- 
-// var keen = keenConfigure;
-var keen = Keen.configure(keenConfigure);
+// var wifiSettings = {ssid: 'Hack Reactor', password:'awesomebullets'};
+var wifiNetwork = 'Hack Reactor';
+var wifiPassword = 'awesomebullets';
+var wifiTimeoutTime = 0; //in seconds
+var wifiSecurity  = 'wpa2';
 
+var wifiTimeouts = 0;
+
+var keen = Keen.configure(keenConfigure);
 // src colony modules tls.js
  
 var tessel = require('tessel');
@@ -20,6 +24,8 @@ var ambient = ambientlib.use(tessel.port.D);
 var THLSinterval = 10; //update interval for temperature, humidty, light and sound
 var soundTriggerLevel = 0.1;
 var lightTriggerLevel = 0.5;
+var led1 = tessel.led[0].output(0);
+var led2 = tessel.led[1].output(0); //led2 is on when wifi is on
 
  
 //------------------------------------------------
@@ -32,6 +38,7 @@ climate.on('ready', function () {
  
     // Loop forever
     setInterval(function () {
+      led1.toggle();//toggle LEDs to know its running
       climate.readTemperature('f', function (err, temp) {
         climate.readHumidity(function (err, humid) {
           ambient.getLightLevel( function (err, light) {
@@ -87,7 +94,7 @@ climate.on('ready', function () {
         sendSoundTrigger(data);
 
       } else {
-        console.log("nope not connected");
+        console.log("wifi is not connected");
       }
       // Clear it
       ambient.clearSoundTrigger();
@@ -139,28 +146,111 @@ function sendSoundTrigger(data){
   });
 }
 
-wifi.on('connect', function() {
-  console.log('Tessel is connected to wifi');
+//------------------------------------------------
+//  Wifi logic 
+//------------------------------------------------
+
+wifi.on('connect', function(data){
+  // you're connected
+  console.log("wifi connect emitted", data);
+  led2 = tessel.led[1].output(1);
 });
 
-wifi.on('disconnect', function (err) {
-  console.log("********** WIFI DISCONNECT ***********");
-  wifi.reset(function() {
-    console.log('reset wifi... attempting reconnect.');
-    wifi.connect(wifiSettings, connectCallback);
-  });
+wifi.on('disconnect', function(data){
+  // wifi dropped, probably want to call connect() again
+  console.log("wifi disconnect emitted", data);
+  led2 = tessel.led[1].output(0);
+  wifiConnect();
 });
 
-wifi.on('error', function(err) {
-  console.log('error with wifi', err);
+wifi.on('timeout', function(err){
+  // tried to connect but couldn't, retry
+  console.log("wifi timeout (tried to connect but couldn't) emitted");
+  wifiTimeouts++;
+  if (wifiTimeouts > 2) {
+    // reset the wifi chip if we've timed out too many times
+    wifiPowerCycle();
+  } else {
+    // try to reconnect
+    wifiConnect();
+  }
 });
+
+wifi.on('error', function(err){
+  // one of the following happened
+  // 1. tried to disconnect while not connected
+  // 2. tried to disconnect while in the middle of trying to connect
+  // 3. tried to initialize a connection without first waiting for a timeout or a disconnect
+  console.log("wifi error emitted", err);
+});
+
+// reset the wifi chip progammatically
+function wifiPowerCycle(){
+  // when the wifi chip resets, it will automatically try to reconnect
+  // to the last saved network
+  wifi.reset(function(){
+    wifiTimeouts = 0; // reset timeouts
+    console.log("done power cycling wifi");
+    // give it some time to auto reconnect
+    setTimeout(function(){
+      if (!wifi.isConnected()) {
+        // try to reconnect
+        wifiConnect();
+      }
+      }, 20 *1000); // 20 second wait
+  })
+}
 
 function connectCallback(data) {
   console.log('reconnected to wifi', data);
 }
 
-if (!wifi.isConnected()) {
-  console.log('connecting to wifi...');
-  wifi.connect(wifiSettings, connectCallback);
+function wifiConnect(){
+  console.log('running wifiConnect');
+  wifi.connect({
+  // security: wifiSecurity, 
+  ssid: wifiNetwork
+  , password: wifiPassword
+  // , timeout: wifiTimeoutTime// in seconds
+  }, connectCallback );
 }
 
+// connect wifi now, if not already connected
+if (!wifi.isConnected()) {
+  console.log('running initial wifiConnect');
+  wifiConnect();
+}else{
+  led2 = tessel.led[1].output(1);
+}
+
+// // wifi.on('disconnect', function(){
+// //   console.log("disconnected, trying to reconnect");
+// //   wifi.connect({
+// //     ssid: 'Hack Reactor',
+// //     password:'awesomebullets'
+// //   });
+// // });
+// wifi.on('connect', function() {
+//   console.log('Tessel is connected to wifi');
+// });
+
+// wifi.on('disconnect', function (err) {
+//   console.log("********** WIFI DISCONNECT ***********");
+//   wifi.reset(function() {
+//     console.log('reset wifi... attempting reconnect.');
+//     wifi.connect(wifiSettings, connectCallback);
+//   });
+// });
+
+// wifi.on('error', function(err) {
+//   console.log('error with wifi', err);
+// });
+
+// function connectCallback(data) {
+//   console.log('reconnected to wifi', data);
+// }
+
+// if (!wifi.isConnected()) {
+//   console.log('connecting to wifi...');
+//   wifi.connect(wifiSettings, connectCallback);
+// }
